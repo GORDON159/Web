bfd65e76b639d8a4d99e536e18207da296ff2dc5
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-07 04:39:50 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -34,10 +34,6 @@ cursor.execute(sql)
 result=cursor.fetchall()
 result= pd.DataFrame(result)
 result.columns=['exercise','grade','suggest','flag','TIME','name','times']
-line_chart_data = result['grade']
-line_chart_data = pd.DataFrame(line_chart_data)
-line_chart_data.set_index(result['times'],inplace=True)
-line_chart = st.line_chart(line_chart_data,use_container_width = True)
 x = result['times']
 y = result['grade']
 p=figure(
