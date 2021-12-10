a377156358d9873b036e215fee2cab3a9e153078
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-11 01:31:25 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -38,7 +38,7 @@ sql='''SELECT `exercise`, `grade`, `suggest`, `TIME`, `times` FROM Identify wher
 cursor.execute(sql)
 re=cursor.fetchall()
 list1 = list(re)
-re= pd.DataFrame(re, columns=('運動','分數','建議','日期','次數'))
+re= pd.DataFrame(re, columns=('運動','次數','分數','建議','日期'))
 
 st.table(re)
 try:
