b4f0318a49f95d648ffa4e37a37af385839552d8
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-14 15:15:58 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -54,7 +54,7 @@ try:
     y_axis_label='分數',
     title='二頭彎舉')
     p.title.text_font_size = '40pt'
-    p.title_location='top',
+    p.title_location='above',
     p.xaxis.axis_label_text_font_size = "30pt"
     p.xaxis.axis_label_text_font_style = "bold"
     p.xaxis.major_label_text_font_size = "15pt"
