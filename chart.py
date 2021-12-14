8804b900528bf7be3c722cb31b4d26f7b0bbb54d
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-14 14:44:15 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -52,7 +52,9 @@ try:
     p=figure(
     x_axis_label='次數',
     y_axis_label='分數')
+    print("~")
     p.x_axis_label_text_font_size = "30"
+    print("123")
     p.x_axis_label_text_font_style = "bold"
     p.y_axis_label_text_font_size = "30"
     p.y_axis_label_text_font_style = "bold"
