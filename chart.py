e9fa420058b5efe5ebd02f2c1574232cb478ae03
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-14 14:59:37 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -52,13 +52,15 @@ try:
     p=figure(
     x_axis_label='次數',
     y_axis_label='分數')
-    st.write("沒")
+    
     p.xaxis.axis_label_text_font_size = "30pt"
-    st.write("有")
     p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "25pt"
+    
     p.yaxis.axis_label_text_font_size = "30pt"
     p.yaxis.axis_label_text_font_style = "bold"
-    p.line(x, y, legend_label='Score', line_width=2)
+    p.yaxis.major_label_text_font_size = "25pt"
+    p.line(x, y, legend_label='Score', line_width=3)
     st.bokeh_chart(p, use_container_width=False)
 except:
     st.write("沒有資料")
