3901665592ecbb77b918c999c3a1ea1d4329e57a
GORDON159 <77201869+GORDON159@users.noreply.github.com>
2021-12-18 22:57:01 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -42,7 +42,7 @@ re= pd.DataFrame(re, columns=('運動','次數','分數','建議','日期'))
 
 st.table(re)
 try:
-    sql = '''SELECT*FROM Identify where exercise='二頭彎舉' and TIME='%s';'''%(str(d))
+    sql = '''SELECT*FROM Identify where exercise='二頭彎舉' and TIME='%s' and name='%s';'''%(str(d),result[0])
     cursor.execute(sql)
     result=cursor.fetchall()
     result= pd.DataFrame(result)
@@ -64,4 +64,124 @@ try:
     p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
     st.bokeh_chart(p, use_container_width=False)
 except:
-    st.write("沒有資料")
+    st.write("二頭彎舉沒有資料")
+try:
+    sql = '''SELECT*FROM Identify where exercise='平舉(左)' and TIME='%s' and name='%s';'''%(str(d),result[0])
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result= pd.DataFrame(result)
+    result.columns=['exercise','grade','suggest','flag','TIME','name','times']
+    x = result['times']
+    y = result['grade']
+    p=figure(
+    x_axis_label='次數',
+    y_axis_label='分數',
+    title='平舉(左)')
+    p.title.text_font_size = '30pt'
+    p.title.align= "center"
+    p.xaxis.axis_label_text_font_size = "20pt"
+    p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "20pt"
+    p.yaxis.axis_label_text_font_style = "bold"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
+    st.bokeh_chart(p, use_container_width=False)
+except:
+    st.write("平舉(左)沒有資料")
+try:
+    sql = '''SELECT*FROM Identify where exercise='平舉(右)' and TIME='%s' and name='%s';'''%(str(d),result[0])
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result= pd.DataFrame(result)
+    result.columns=['exercise','grade','suggest','flag','TIME','name','times']
+    x = result['times']
+    y = result['grade']
+    p=figure(
+    x_axis_label='次數',
+    y_axis_label='分數',
+    title='平舉(右)')
+    p.title.text_font_size = '30pt'
+    p.title.align= "center"
+    p.xaxis.axis_label_text_font_size = "20pt"
+    p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "20pt"
+    p.yaxis.axis_label_text_font_style = "bold"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
+    st.bokeh_chart(p, use_container_width=False)
+except:
+    st.write("平舉(右)沒有資料")
+try:
+    sql = '''SELECT*FROM Identify where exercise='相撲式硬舉' and TIME='%s' and name='%s';'''%(str(d),result[0])
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result= pd.DataFrame(result)
+    result.columns=['exercise','grade','suggest','flag','TIME','name','times']
+    x = result['times']
+    y = result['grade']
+    p=figure(
+    x_axis_label='次數',
+    y_axis_label='分數',
+    title='相撲式硬舉')
+    p.title.text_font_size = '30pt'
+    p.title.align= "center"
+    p.xaxis.axis_label_text_font_size = "20pt"
+    p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "20pt"
+    p.yaxis.axis_label_text_font_style = "bold"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
+    st.bokeh_chart(p, use_container_width=False)
+except:
+    st.write("相撲式硬舉沒有資料")
+try:
+    sql = '''SELECT*FROM Identify where exercise='弓箭步伸展(左)' and TIME='%s' and name='%s';'''%(str(d),result[0])
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result= pd.DataFrame(result)
+    result.columns=['exercise','grade','suggest','flag','TIME','name','times']
+    x = result['times']
+    y = result['grade']
+    p=figure(
+    x_axis_label='次數',
+    y_axis_label='分數',
+    title='弓箭步伸展(左)')
+    p.title.text_font_size = '30pt'
+    p.title.align= "center"
+    p.xaxis.axis_label_text_font_size = "20pt"
+    p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "20pt"
+    p.yaxis.axis_label_text_font_style = "bold"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
+    st.bokeh_chart(p, use_container_width=False)
+except:
+    st.write("弓箭步伸展(左)沒有資料")
+try:
+    sql = '''SELECT*FROM Identify where exercise='弓箭步伸展(右)' and TIME='%s' and name='%s';'''%(str(d),result[0])
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result= pd.DataFrame(result)
+    result.columns=['exercise','grade','suggest','flag','TIME','name','times']
+    x = result['times']
+    y = result['grade']
+    p=figure(
+    x_axis_label='次數',
+    y_axis_label='分數',
+    title='弓箭步伸展(右)')
+    p.title.text_font_size = '30pt'
+    p.title.align= "center"
+    p.xaxis.axis_label_text_font_size = "20pt"
+    p.xaxis.axis_label_text_font_style = "bold"
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "20pt"
+    p.yaxis.axis_label_text_font_style = "bold"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.line(x, y, legend_label='Score', line_width=4 , line_color = 'black')
+    st.bokeh_chart(p, use_container_width=False)
+except:
+    st.write("弓箭步伸展(右)沒有資料")
